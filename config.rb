ff6c3ff58eaa13b72d310f74a4d4820dfa01bb53
seanahrens <seanahrens@gmail.com>
2014-03-05 00:15:37 -0800

    -u

--- config.rb
+++ config.rb
@@ -67,4 +67,22 @@ configure :build do
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
+
+  # To deploy to a remote branch via git (e.g. gh-pages on github):
+  activate :deploy do |deploy|
+    deploy.method = :git
+    # remote is optional (default is "origin")
+    # run `git remote -v` to see a list of possible remotes
+    deploy.remote = "github"
+
+    # branch is optional (default is "gh-pages")
+    # run `git branch -a` to see a list of possible branches
+    # deploy.branch = "some-other-branch-name"
+
+    # strategy is optional (default is :force_push)
+    # deploy.strategy = :submodule
+
+    # To automatically run middleman build during middleman deploy, turn on the build_before option while activating the deploy extension:
+    deploy.build_before = true
+  end
 end
